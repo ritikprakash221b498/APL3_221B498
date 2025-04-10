0ab3e5d688f12ae17d97a8323c9b605eabe35041
221B498 <221B498@juet.co.in>
2025-04-10 15:16:56 +0530

    This is my sixth commit

---
+++ LE-6/Main.java
@@ -0,0 +1,10 @@
+class Main{
+    public static void main(String args[]){
+        Offering offering=new Coffee();
+        offering=new Rum(offering);
+        System.out.println(offering.getName()+" "+offering.getPrice());
+        Offering offering1=new Tea();
+        offering1=new Rum(offering1);
+        System.out.println(offering1.getName()+" "+offering1.getPrice());
+    }
+}
